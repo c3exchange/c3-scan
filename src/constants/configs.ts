c5d14b06b0f0634df55ab8ed0e302f023e6e32e5
Lucas Assmann <l.assmann.30@gmail.com>
2023-11-24 14:56:56 -0300

    Add mainnet configs (#1)

--- src/constants/configs.ts
+++ src/constants/configs.ts
@@ -1,14 +1,14 @@
 const configs = {
   mainnet: {
-    c3ApiUrl: 'https://api.test.c3.io/v1',
-    indexer: 'https://testnet-idx.algonode.cloud',
-    algoNode: 'https://node.testnet.algoexplorerapi.io',
+    c3ApiUrl: 'https://api.c3.io/v1',
+    indexer: 'https://mainnet-idx.algonode.cloud',
+    algoNode: 'https://mainnet-api.algonode.cloud',
     isMainnet: true,
   },
   testnet: {
     c3ApiUrl: 'https://api.test.c3.io/v1',
     indexer: 'https://testnet-idx.algonode.cloud',
-    algoNode: 'https://node.testnet.algoexplorerapi.io',
+    algoNode: 'https://testnet-api.algonode.cloud',
     isMainnet: false,
   },
 };
