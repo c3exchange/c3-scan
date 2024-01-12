4f1f68c9e40c850a9f319dba82673bbd73549113
Lucas Assmann <l.assmann.30@gmail.com>
2024-01-12 13:07:49 -0300

    Add google tag manager (#12)

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import Layout from './Layout';
 import { GlobalContextProvider } from './contexts/GlobalContext';
 import { useEffect, useState } from 'react';
 import PersistQueryParam from './routes/PersistQueryParam';
+import ReactGA from 'react-ga4';
 
 function App() {
   const [shouldPersistEnv, setShouldPersistEnv] = useState(false);
-
+  ReactGA.initialize('G-22FJC5G2E2');
   useEffect(() => {
     const queryParams = new URLSearchParams(window.location.search);
     if (queryParams.get('env') === 'test') setShouldPersistEnv(true);
