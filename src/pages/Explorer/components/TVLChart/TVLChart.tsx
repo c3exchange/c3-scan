f882901aa454c2b142ca5f0487e6741769519d0e
MatHermida <hermidamatias2@gmail.com>
2024-06-19 13:40:09 -0300

    rework mobile

--- src/pages/Explorer/components/TVLChart/TVLChart.tsx
+++ src/pages/Explorer/components/TVLChart/TVLChart.tsx
@@ -1,8 +1,8 @@
 import { useMemo } from 'react';
 import { useWindowSize } from '../../../../hooks/useWindowSize';
 import { breakpoints } from '../../../../theme';
-import MobileTVLChart from './mobile/MobileTVLChart';
-import DesktopTVLChart from './desktop/DesktopTVLChart';
+
+import * as S from './styles';
 
 const TVLChart = () => {
   const windowSize = useWindowSize();
@@ -11,7 +11,37 @@ const TVLChart = () => {
     [windowSize.width]
   );
 
-  return <>{isMobile ? <MobileTVLChart /> : <DesktopTVLChart />}</>;
+  return (
+    <>
+      {isMobile ? (
+        <S.TVLChartContainerMobile item mobile={12}>
+          <S.TVLChartTitleMobile item>Total Value Locked Chart</S.TVLChartTitleMobile>
+          <S.TVLChartMobile>
+            <iframe
+              width="100%"
+              height="360px"
+              src="https://defillama.com/chart/protocol/c3-exchange?tvl=true&denomination=USD&theme=dark"
+              title="C3"
+              frameBorder="0"
+            ></iframe>
+          </S.TVLChartMobile>
+        </S.TVLChartContainerMobile>
+      ) : (
+        <S.TVLChartContainerDesktop item mobile={12}>
+          <S.TVLChartTitleDesktop item>Total Value Locked Chart</S.TVLChartTitleDesktop>
+          <S.TVLChartDesktop>
+            <iframe
+              width="100%"
+              height="360px"
+              src="https://defillama.com/chart/protocol/c3-exchange?tvl=true&denomination=USD&theme=dark"
+              title="C3"
+              frameBorder="0"
+            ></iframe>
+          </S.TVLChartDesktop>
+        </S.TVLChartContainerDesktop>
+      )}
+    </>
+  );
 };
 
 export default TVLChart;
